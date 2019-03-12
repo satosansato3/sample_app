14474e0eafecf11b78401964bd3788672e7578a8
satosansato3 <koji.sato1242@gmail.com>
2019-03-12 16:37:18 +0000

    index page showing only activated user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    #@users = User.where(activated: true).paginate(page: params[:page])
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def show
