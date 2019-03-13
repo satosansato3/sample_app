de2a89d9c7f2e7eec20cd437c74a42c40f1bccd6
satosansato3 <koji.sato1242@gmail.com>
2019-03-13 13:38:32 +0000

    host changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://warm-taiga-65344.herokuapp.com'
+  host = 'warm-taiga-65344.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
