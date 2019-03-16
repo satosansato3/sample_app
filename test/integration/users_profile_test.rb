f22c808d5b2a08ed640e949202b82f3443ab6c60
satosansato3 <koji.sato1242@gmail.com>
2019-03-16 17:34:10 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,5 +19,6 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
     assert_select 'div.pagination', count: 1
+    
   end
 end
